0f62760966a7ff1104b843f22bbbe31762639071
Michael Alekseew <alekseew.m@googlemail.com>
2022-06-19 21:08:44 +0200

    refactor(FE): move app.module in own folder in FE

--- apps/bobbycar-race-bammental/src/app/app/app.component.ts
+++ apps/bobbycar-race-bammental/src/app/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Message } from '@alekseew-arduino/api-interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'alekseew-arduino-root',
