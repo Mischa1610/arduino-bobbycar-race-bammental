ba7161fe90897a9471ec1fdb96dd9fc34d614a8d
Michael Alekseew <alekseew.m@googlemail.com>
2022-06-23 19:05:02 +0200

    feat(FE): Feature/16 add angular material cdk (#23)
    * feat(FE-app): add Angular Material + CDK inital dependencys
    
    * feat(FE-nav): :sparkles: add responsive side navigation
    
    * feat(root): update & refresh all dependencys in package + package-lock
    
    * refactor(BE): use global api prefix /api/v1
    
    * build(root): set firebase hosting path to apps/bobbycar-race-bammental
    
    Closes #16, Closes #23

--- apps/bobbycar-race-bammental/src/app/app/app.component.ts
+++ apps/bobbycar-race-bammental/src/app/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Message } from '@alekseew-arduino/api-interfaces';
+import { API_PREFIX, Message } from '@alekseew-arduino/api-interfaces';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Links } from '../main-navigation/main-navigation.component';
 
 @Component({
   selector: 'alekseew-arduino-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  protected readonly APP_TITLE = 'Bammentaler Bobbycar-Rennen';
+  protected readonly MENU_TITLE = 'Menu';
+
+  protected readonly LINKS: Links[] = [
+    { link: '#', title: 'Home' },
+    { link: '/times', title: 'Rennzeiten', disabled: true },
+    { link: '/rankings', title: 'Ranglisten', disabled: true },
+  ];
+
+  hello$ = this.http.get<Message>(`/${API_PREFIX}/hello`);
   constructor(private http: HttpClient) {}
 }
