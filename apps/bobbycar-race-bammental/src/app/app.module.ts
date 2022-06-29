d5d180655659290193144fbde45e5fa63a843596
Michael Alekseew <michael.alekseew@sovanta.com>
2022-06-30 01:43:45 +0200

    refactor(FE): :recycle: use module arrays for imports for angular, firebase & material

--- apps/bobbycar-race-bammental/src/app/app.module.ts
+++ apps/bobbycar-race-bammental/src/app/app.module.ts
@@ -1,23 +1,8 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {
-  getAnalytics,
-  provideAnalytics,
-  ScreenTrackingService,
-  UserTrackingService,
-} from '@angular/fire/analytics';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
-import {
-  getRemoteConfig,
-  provideRemoteConfig,
-} from '@angular/fire/remote-config';
-import { getStorage, provideStorage } from '@angular/fire/storage';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
@@ -34,6 +19,34 @@ import { MainNavigationComponent } from './main-navigation/main-navigation.compo
 import { SwitchTranslationComponent } from './switch-translation/switch-translation.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
+const ANGULAR_MODULES = [
+  BrowserModule,
+  HttpClientModule,
+  BrowserAnimationsModule,
+];
+
+const FIREBASE_MODULES = [
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  //provideAnalytics(() => getAnalytics()),
+  //provideAuth(() => getAuth()),
+  provideDatabase(() => getDatabase()),
+  //provideFirestore(() => getFirestore()),
+  //provideMessaging(() => getMessaging()),
+  //providePerformance(() => getPerformance()),
+  //provideRemoteConfig(() => getRemoteConfig()),
+  //provideStorage(() => getStorage()),
+];
+
+const ANGULAR_MATERIAL_MODULES = [
+  LayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,28 +55,14 @@ import { TranslocoRootModule } from './transloco-root.module';
     SwitchTranslationComponent,
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAnalytics(() => getAnalytics()),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideStorage(() => getStorage()),
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
+    ...ANGULAR_MODULES,
+    ...FIREBASE_MODULES,
+    ...ANGULAR_MATERIAL_MODULES,
     TranslocoRootModule,
   ],
-  providers: [ScreenTrackingService, UserTrackingService],
+  providers: [
+    /*ScreenTrackingService, UserTrackingService*/
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
